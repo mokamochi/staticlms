2da5c37938ab1fb0d571d663910af59ed5347536
Mokamochi <nanra39s@gmail.com>
2020-11-20 12:54:49 +0700

    Add files via upload

--- assets/js/ixlab.js
+++ assets/js/ixlab.js
@@ -4,6 +4,12 @@
 $('.ix-navbar-dropdown a span').clone().prependTo('.ix-navbar-dropdown ul li:nth-child(1)');
 $('.ix-grid-3 .ix-grid-item:nth-child(3n+3)').after('<div class="clear"></div>');
 
+$('.section-isi-materi ul.section li.activity:has(.contentwithoutlink) .actions').remove();
+$('.my-course-grid-profile-listing .ix-grid-item:nth-child(n+5)').hide();
+$('.ix-profile-c-more').on('click', function () {
+  $('.my-course-grid-profile-listing .ix-grid-item:nth-child(n+5)').toggle();
+});
+
 $('.mk-history li:nth-child(1) i').clone().prependTo('.mk-history li');
 $('.kb-list li:nth-child(1) i').clone().prependTo('.kb-list li');
 $('.task-list li:nth-child(1) i').clone().prependTo('.task-list li');
@@ -22,10 +28,28 @@ $(document).mouseup(function (e) {
 });
 
 $(function() {
-  $('.ix-navbar-menu ul li a').each(function() {
+  $('.ix-navbar-menu ul.satu li a').each(function() {
     if ($(this).prop('href') == window.location.href) {
       $(this).addClass('active');
       $(this).parents('li').addClass('active');
     }
   });
 });
+
+$(document).ready(function() {
+  $('.colsx')
+    .theiaStickySidebar({
+      additionalMarginTop: 88,
+      additionalMarginBottom: 0
+    });
+});
+
+$('.ix-course-week ul li a').on('click', function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 0);
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
